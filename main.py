3a2afc08e1f09fac21168cfeb508344724bb84e2
Andi <ic20b050@technikum-wien.at>
2023-02-24 21:23:17 +0100

    add post function

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ print("")
 print("Possible calls:")
 print("http://localhost:8000/")
 print("GET: http://localhost:8000/hello/ic20b050")
+print("POST: http://localhost:8000/hello  name=ic20b050");
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World! I'm the FastAPI in Python."}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
-    return {"message": f"Hello {name}!"}
+    return {"message": f"Hello {name}! I'm the FastAPI in Python."}
+
+
+@app.post("/hello/{name}")
+async def create_item(name: str):
+    return {"message": f"Hello {name}! I'm the FastAPI in Python."}
