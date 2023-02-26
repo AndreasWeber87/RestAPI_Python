f17bc7e9b892bcd87c7b68e04e87756c2c5bfbe9
Andi <ic20b050@technikum-wien.at>
2023-02-26 22:49:49 +0100

    add database query

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+# Sources:
+# https://fastapi.tiangolo.com
+# https://magicstack.github.io/asyncpg/current/index.html
+import sys
+
+from fastapi import FastAPI, HTTPException
+import asyncpg
 
 app = FastAPI()
 print("Server started on port 8000...")
@@ -6,7 +12,9 @@ print("")
 print("Possible calls:")
 print("http://localhost:8000/")
 print("GET: http://localhost:8000/hello/ic20b050")
-print("POST: http://localhost:8000/hello  name=ic20b050");
+print("POST: http://localhost:8000/hello/name  name=ic20b050")
+print("")
+print("GET: http://localhost:8000/getGemeinde?id=10101")
 
 
 @app.get("/")
@@ -22,3 +30,22 @@ async def say_hello(name: str):
 @app.post("/hello/{name}")
 async def create_item(name: str):
     return {"message": f"Hello {name}! I'm the FastAPI in Python."}
+
+
+@app.get("/getGemeinde")
+async def say_hello(id: int):
+    user = "postgres"
+    password = "xsmmsgbAMfIOIWPPBrsc"
+    host = "127.0.0.1"
+    port = "5432"
+    database = "ogd"
+
+    try:
+        conn = await asyncpg.connect(f'postgresql://{user}:{password}@{host}:{port}/{database}')
+        row = await conn.fetchrow(
+            'SELECT gemeindename FROM public.gemeinde WHERE gkz=$1 LIMIT 1', id)
+        await conn.close()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {"gemeindename": row}
