493c0975b8da7359112ceeacc4d760c08c4d6eaa
Andi <ic20b050@technikum-wien.at>
2023-03-22 01:29:19 +0100

    Port geändert

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ from fastapi import FastAPI, HTTPException
 import asyncpg
 
 app = FastAPI()
-print("Server started on port 8000...")
+print("Server started on port 9000...")
 print("")
 print("Possible calls:")
-print("http://localhost:8000/")
-print("GET: http://localhost:8000/hello/ic20b050")
-print("POST: http://localhost:8000/hello/name  name=ic20b050")
+print("http://localhost:9000/")
+print("GET: http://localhost:9000/hello/ic20b050")
+print("POST: http://localhost:9000/hello/name  name=ic20b050")
 print("")
-print("GET: http://localhost:8000/getGemeinde?id=10101")
+print("GET: http://localhost:9000/getGemeinde?id=10101")
 
+conn = None
 
 @app.get("/")
 async def root():
@@ -33,18 +34,21 @@ async def create_item(name: str):
 
 @app.get("/getGemeinde")
 async def say_hello(id: int):
-    user = "postgres"
-    password = "xsmmsgbAMfIOIWPPBrsc"
-    # host = "127.0.0.1"
-    host = "192.168.0.2"  # container ip
-    port = "5432"
-    database = "ogd"
-
     try:
-        conn = await asyncpg.connect(f'postgresql://{user}:{password}@{host}:{port}/{database}')
+        global conn
+
+        if conn is None:
+            user = "postgres"
+            password = "xsmmsgbAMfIOIWPPBrsc"
+            host = "127.0.0.1"
+            # host = "192.168.0.2"  # container ip
+            port = "5432"
+            database = "ogd"
+            conn = await asyncpg.connect(f'postgresql://{user}:{password}@{host}:{port}/{database}')
+
         row = await conn.fetchrow(
             'SELECT gemeindename FROM public.gemeinde WHERE gkz=$1 LIMIT 1', id)
-        await conn.close()
+        # await conn.close()
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
